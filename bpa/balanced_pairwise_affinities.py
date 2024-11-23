Fixed distance computation

bpa/balanced_pairwise_affinities.py
@@ -34,18 +34,20 @@ class BPA(nn.Module):
         self.sigmoid = sigmoid
         self.ot_reg = ot_reg
         self.max_scale = max_scale
-        self.diagonal_val = 1e3                         # value to mask self-values with
+        self.diagonal_val = 1e5                         # value to mask self-values with
 
     def compute_cost_matrix(self, x: Tensor) -> Tensor:
         """
         Compute the cost matrix.
         """
-        # euclidean
+        # Euclidean distances
         if self.distance_metric == 'euclidean':
+            # dim_offset = 0 if x.dim() <= 2 else 1
+            # pairwise_dist = (x.unsqueeze(1+dim_offset) - x.unsqueeze(0+dim_offset)).norm(dim=-1).pow(2)
             pairwise_dist = torch.cdist(x, x, p=2)
-            # scale euclidean distances to [0, 1]
-            pairwise_dist = pairwise_dist / pairwise_dist.max()
-        # cosine
+            pairwise_dist = pairwise_dist / pairwise_dist.max()  # scale distances to [0, 1]
+
+        # Cosine distances
         elif self.distance_metric == 'cosine':
             x_norm = F.normalize(x, dim=-1, p=2)
             pairwise_dist = 1 - (x_norm @ x_norm.transpose(-2, -1))

methods/prototypical/proto_loss.py
@@ -5,11 +5,14 @@ from bpa import BPA
 
 
 class ProtoLoss(nn.Module):
+    """
+    Prototypical loss function
+    """
     def __init__(self, args: dict, bpa: BPA = None):
         super().__init__()
-        self.way_dict = dict(train=args['train_way'], val=args['val_way'])
         self.num_shot = args['num_shot']
         self.num_query = args['num_query']
+        self.way_dict = dict(train=args['train_way'], val=args['val_way'])
         self.temperature = args['temperature']
         self.BPA = bpa
         self.num_labeled = None
@@ -30,14 +33,16 @@ class ProtoLoss(nn.Module):
         if self.BPA is not None:
             X = self.BPA(X)
 
-        X_s, X_q = X[:self.num_labeled], X[self.num_labeled:]
+        # split to support and queries
+        X_support, X_query = X.split((self.num_labeled, X.size(0)-self.num_labeled), dim=0)
 
         # compute centroids
-        # assuming input data is sorted as [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, ...]
-        X_c = X_s.reshape(self.num_shot, num_way, -1).transpose(0, 1).mean(dim=1)
+        # -> assuming input data sorted as [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, ...]
+        X_centroid = X_support.reshape(self.num_shot, num_way, -1).transpose(0, 1).mean(dim=1)
 
         # compute distances between queries and the centroids
-        D = torch.cdist(X_q, X_c) / self.temperature
+        D = (X_query.unsqueeze(1) - X_centroid.unsqueeze(0)).norm(dim=2).pow(2)
+        D = D / self.temperature
 
         return -D, ProtoLoss.get_accuracy(D, labels)
 

methods/pt_map/pt_map_loss.py
@@ -60,7 +60,8 @@ class GaussianModel:
         return P
 
     def get_probas(self, X: torch.Tensor, labels: torch.Tensor):
-        dist = torch.cdist(X, self.mus)
+        # compute squared dist to centroids [n_samples][n_ways]
+        dist = (X.unsqueeze(1)-self.mus.unsqueeze(0)).norm(dim=2).pow(2)
         p_xj = torch.zeros_like(dist)
         r = torch.ones(1, self.num_way * self.num_query, device='cuda')
         c = torch.ones(1, self.num_way, device='cuda') * self.num_query